f4a76c7359d351fd8b01b05b26afefe7a896f13f
Long <mt-fa29@nidec-copal.vn>
2018-09-21 13:29:15 +0700

    update

--- IPQC Motor/frmItemMaster.cs
+++ IPQC Motor/frmItemMaster.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Globalization;
-using System.Security.Permissions;
-using System.Collections;
+using System.IO;
+using System.Threading.Tasks;
+using System.Management;
+using System.Drawing.Drawing2D;
 using Npgsql;
 
 namespace IPQC_Motor
@@ -33,8 +36,8 @@ namespace IPQC_Motor
         public DataTable dt;
         public void LoadItem()
         {
-            drw_txt.Text = DrawingCd;
-            string sql = "select item_id,a.dwr_id , item_measure,  item_symbol,item_tool,item_row,item_detail,item_spec_x,  item_lower, item_upper   from m_item a left join m_drawing b on a.dwr_id = b.dwr_id where b.dwr_cd = '" + DrawingCd + "'  ";
+            txtDwr.Text = DrawingCd;
+            string sql = "select item_id,a.dwr_id , item_measure,  item_symbol,item_tool,item_row,item_detail,item_spec_x,  item_lower, item_upper  from m_item a left join m_drawing b on a.dwr_id = b.dwr_id where b.dwr_cd = '" + DrawingCd + "'  ";
             TfSQL tfSql = new TfSQL();
             dt = new DataTable();
             tfSql.sqlDataAdapterFillDatatable(sql, ref dt);
@@ -56,7 +59,23 @@ namespace IPQC_Motor
             dgvTester.Columns["item_detail"].HeaderText = "Detail";
             dgvTester.Columns["item_row"].HeaderText = "Row";
 
-            dgvTester.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvTester.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            //dgvTester.Columns["item_measure"].AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            dgvTester.Columns["item_measure"].Width = 100;
+
+            //Load Image to picture box
+
+            string bytePic = tfSql.sqlExecuteScalarString("select dwr_image from m_drawing where dwr_cd = '" + DrawingCd + "'");
+            if (bytePic != "")
+            {
+                byte[] imgBytes = Convert.FromBase64String(bytePic);
+                MemoryStream ms = new MemoryStream(imgBytes, 0, imgBytes.Length);
+                ms.Write(imgBytes, 0, imgBytes.Length);
+                Image image = Image.FromStream(ms, true);
+
+                picbox.Image = image;
+                picbox.SizeMode = PictureBoxSizeMode.Zoom;
+            }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -78,12 +97,11 @@ namespace IPQC_Motor
                 if (dialog == DialogResult.Yes)
                 {
                     TfSQL tfsql = new TfSQL();
-                    bool del = true;
-                    //string dwr_cd = dgvTester.Rows[rowIndex].Cells["dwr_cd"].Value.ToString();
+
                     int item_measure = int.Parse(dgvTester.Rows[rowIndex].Cells["item_measure"].Value.ToString());
                     string sqlDelete = @"delete from m_item where dwr_id = (select dwr_id from m_drawing where dwr_cd = '" + DrawingCd +
                         "') and item_measure = " + item_measure;
-                    tfsql.sqlExecuteNonQuery(sqlDelete, del);
+                    bool del = tfsql.sqlExecuteNonQuery(sqlDelete, true);
                     LoadItem();
                 }
             }
@@ -133,6 +151,71 @@ namespace IPQC_Motor
                 {
                     e.Cancel = true;
                 }
+            }
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "Open Image File";
+            file.Filter = "Image files|*.jpg;*.png";
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                if (file.FileName != "")
+                {
+                    txtLink.Text = file.FileName.ToString();
+                    long size = new System.IO.FileInfo(txtLink.Text).Length / 1024;
+                    bool doing = false;
+                    if (size > 310)
+                    {
+                        DialogResult dialog = MessageBox.Show("File size is greater than 300 Kb !" + System.Environment.NewLine + "Do you want continue!", "Note!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (dialog == DialogResult.Yes)
+                        {
+                            doing = true;
+                        }
+                    }
+                    else doing = true;
+
+                    if (doing == true)
+                    {
+
+                        byte[] img = System.IO.File.ReadAllBytes(txtLink.Text);
+                        MemoryStream meStream = new MemoryStream(img);
+
+                        picbox.Image = new Bitmap(meStream);
+                        picbox.SizeMode = PictureBoxSizeMode.Zoom;
+
+                        doing = false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No file selected!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtLink.Text = "";
+                }
+            }
+        }
+
+        private void btnSaveImg_Click(object sender, EventArgs e)
+        {
+            if (txtLink.Text != "")
+            {
+                FileStream fs = new FileStream(txtLink.Text, FileMode.Open, FileAccess.Read);
+                byte[] pic = new byte[fs.Length];
+                fs.Read(pic, 0, System.Convert.ToInt32(fs.Length));
+                fs.Close();
+
+                TfSQL sql = new TfSQL();
+                int dwrId = int.Parse(sql.sqlExecuteScalarString("select dwr_id from m_drawing where dwr_cd = '" + DrawingCd + "'"));
+                string sqlEx = "update m_drawing set dwr_image = '" + Convert.ToBase64String(pic) + "' where dwr_id = " + dwrId;
+                bool bsave = true;
+                sql.sqlExecuteNonQueryInt(sqlEx, bsave);
+            }
+            else
+            {
+                MessageBox.Show("Select a file !", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtLink.Text = "";
             }
         }
     }
